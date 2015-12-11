8a43ecd721486cb5ec82bd95a977982f966adead
manfred <manfred@adverway.com>
2015-12-11 11:56:50 +0100

    New bio photo

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var gulp         = require('gulp'),
     autoprefixer = require('autoprefixer-stylus'),
     STYL_FILES   = ['assets/styles/**/*.styl'],
     JS_FILES     = ['assets/js/**/*.js'],
-    JADE_FILES   = ['src/views/*.jade'],
+    JADE_FILES   = ['src/views/index.jade'],
     PUBLIC_FILES = ['public/**/*'],
     ENV          = process.env.NODE_ENV || 'development';
 
